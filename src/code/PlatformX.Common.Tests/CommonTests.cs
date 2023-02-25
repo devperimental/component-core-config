ec5fac812fc7d236244358f5aa71d7b65afdf6dd
Christian Farinella <christian.farinella@ai.astra.co.id>
2023-02-25 11:58:37 +1100

    test: add tests

--- src/code/PlatformX.Common.Tests/CommonTests.cs
+++ src/code/PlatformX.Common.Tests/CommonTests.cs
@@ -15,14 +15,40 @@ namespace Useful.Implementation.Tests
         [Test]
         public void TestConfiguration()
         {
-            var config = new BootstrapConfiguration();
+            var config = new BootstrapConfiguration()
+            {
+                Prefix  = "ABC",
+                RoleKey  = "ABC",
+                Environment  = "ABC",
+                Region  = "ABC",
+                Location  = "ABC",
+                PortalName  = "ABC",
+                Layer  = "ABC",
+                LogMessages  = true,
+                LogWarnings  = true,
+                LogErrors  = true,
+                DBName  = "ABC",
+                PlatformServiceMetaData  = "ABC",
+                ServiceKeys  = "ABC",
+                RuntimeConfiguration  = "ABC",
+                TenantId  = "ABC",
+                ServiceSymmetricKeyName  = "ABC",
+                ServerName  = "ABC",
+                BuildNumber  = "ABC",
+                ContainerType  = "ABC"
+            };
+
             Assert.IsNotNull(config);            
         }
 
         [Test]
         public void TestRuntimeSettings()
         {
-            var config = new RuntimeSettings();
+            var config = new RuntimeSettings()
+            {
+                Settings = new Dictionary<string, string> { { "ABC", "DEF"} }
+            };
+
             Assert.IsNotNull(config);
         }
     }
