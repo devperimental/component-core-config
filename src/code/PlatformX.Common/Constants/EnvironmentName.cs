0d7aec81706f221a86ea63276e0f146e6636a4b1
Christian Farinella <christian.farinella@ai.astra.co.id>
2023-03-05 05:27:12 +1100

    feat: Add more fields to BootstrapConfiguration

---
+++ src/code/PlatformX.Common/Constants/EnvironmentName.cs
@@ -0,0 +1,10 @@
+﻿namespace PlatformX.Common.Constants
+{
+    public static class EnvironmentName
+    {
+        public const string Local = "local";
+        public const string Development = "dev";
+        public const string Test = "test";
+        public const string Production = "prod";
+    }
+}
